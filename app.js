652a854b4ddf8c649ead4df3bb0a691261ed99c0
Abhishek Kumar <2021pcecsabhishek010@poornima.org>
2025-04-23 05:51:23 +0530

    final updated code-01

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ connectDB();
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://humanliassigment001.netlify.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true 
